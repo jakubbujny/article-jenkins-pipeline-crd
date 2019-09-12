1a72045d28ad1397883c0cdbe3e6e4c01bf429be
Jakub Bujny <jakub.bujny@j-labs.pl>
2019-09-12 15:36:42 +0200

    update scripts to add trigger and fix seed

--- update-seed-job.sh
+++ update-seed-job.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-curl -s -XPOST 'http://localhost:8080/job/seed/config.xml' -u admin:113e95e04fb410d9e338b209e8e759c2af --data-binary @seed.xml -H "Content-Type:text/xml"
+export TOKEN=1193c34b45d37322d98422fb1f337b5429
+
+curl -s -XPOST 'http://$(minikube ip):30000/job/seed/config.xml' -u admin:113e95e04fb410d9e338b209e8e759c2af --data-binary @seed.xml -H "Content-Type:text/xml"
+
+curl -s -XPOST "http://$(minikube ip):30000/job/seed/buildWithParameters" -u admin:$TOKEN -H "Content-Type:text/xml"
