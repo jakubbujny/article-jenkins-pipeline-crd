0aaed9da96885f6f28424faa25197aa748f41b16
Jakub Bujny <jakub.bujny@j-labs.pl>
2019-09-12 15:44:49 +0200

    fix script

--- update-seed-job.sh
+++ update-seed-job.sh
@@ -2,6 +2,6 @@
 
 export TOKEN=1193c34b45d37322d98422fb1f337b5429
 
-curl -s -XPOST 'http://$(minikube ip):30000/job/seed/config.xml' -u admin:113e95e04fb410d9e338b209e8e759c2af --data-binary @seed.xml -H "Content-Type:text/xml"
+curl -v -XPOST "http://$(minikube ip):30000/job/seed/config.xml" -u admin:$TOKEN --data-binary @seed.xml -H "Content-Type:application/xml"
 
 curl -s -XPOST "http://$(minikube ip):30000/job/seed/buildWithParameters" -u admin:$TOKEN -H "Content-Type:text/xml"
